d592d4cc13160b1c558bd508acc552ba48ed3a12
Mizurkin <samyray2009@yandex.ru>
2022-07-21 19:51:44 +0300

    start layout

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
-<div id="app">
-  <p>Hello Vue</p>
+  <div id="app">
+    <header class="header header--main">
+      <div class="container header__container">
+        <a class="container__img" href="#"
+          ><img src="./img/logo.svg" alt="Easytune logo"
+        /></a>
+      </div>
+    </header>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "./scss/main.scss";
+
+.header {
+  &__container {
+    @include flex-sb;
+  }
 }
 </style>
