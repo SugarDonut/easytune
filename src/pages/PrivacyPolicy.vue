177cde33920195ecafc23abd0b62b603a65b8eaa
Mizurkin <samyray2009@yandex.ru>
2022-09-16 01:56:21 +0300

    fix some scss

--- src/pages/PrivacyPolicy.vue
+++ src/pages/PrivacyPolicy.vue
@@ -3,10 +3,10 @@
     <div class="container">
       <div class="privacy">
         <div class="privacy__title">
-          <h1 class="title">Privacy Policy</h1>
+          <h1 class="title">Privacy<br>Policy</h1>
         </div>
         <div class="privacy__privacy">
-          <p>
+          <p class="privacy__descr">
             The Personal Information Privacy Policy (hereinafter – Policy)
             applies to any and all information about the User that DPF off
             service or its affiliates, including all persons belonging to the
@@ -23,19 +23,62 @@
             contained herein; if the User does not agree with these conditions,
             the User should not use the Services.
           </p>
-          <ol class="privacy__list">
-            <li class="privacy__item">User Personal Information Processed by onlinecarservice.ru</li>
+          <ol class="privacy__list ">
+            <li class="privacy__item privacy__list--head">
+              User Personal Information Processed by onlinecarservice.ru
+            </li>
             <ol class="privacy__list">
-              <li class="privacy__item">According to this Policy, “User personal information” means the following:</li>
+              <li class="privacy__item">
+                According to this Policy, “User personal information” means the
+                following:
+              </li>
               <ol class="privacy__list">
-                <li>Personal information provided by the User during registration (creation of an account) or in the course of using the Services, including the User’s personal details. Information required for the provision of the Services is marked respectively. Other information shall be provided by the User at his/her own discretion.</li>
+                <li class="privacy__item">
+                  Personal information provided by the User during registration
+                  (creation of an account) or in the course of using the
+                  Services, including the User’s personal details. Information
+                  required for the provision of the Services is marked
+                  respectively. Other information shall be provided by the User
+                  at his/her own discretion.
+                </li>
+                <li class="privacy__item">
+                  Data automatically transferred by the software installed on
+                  the User’s device to the onlinecarservice.ru Services in the
+                  course of their use, including, but not limited to, IP
+                  address, cookies data, the User’s browser data (or any other
+                  software used to access the Services), performance
+                  specifications of the User’s hardware and software, date and
+                  time of access to the Services, the URL of the page requested
+                  and other similar information.
+                </li>
+                <li class="privacy__item">
+                  Other information about the User required to be processed
+                  according to the terms and conditions governing the use of
+                  particular onlinecarservice.ru Services.
+                </li>
               </ol>
             </ol>
-            <li class="privacy__item">n</li>
-            <li class="privacy__item">n</li>
-            <li class="privacy__item">n</li>
+            <li class="privacy__item">
+              This Policy only applies to the information required to be
+              processed during the use of onlinecarservice.ru Services.
+              onlinecarservice.ru has no control over, and does not bear
+              responsibility for, processing of the information by third-party
+              websites, which the User may access by hyperlinks available on
+              onlinecarservice.ru’s websites.
+            </li>
+            <li class="privacy__item">
+              onlinecarservice.ru will not verify personal information provided
+              by the User and cannot judge on his/her legal capacity. However,
+              onlinecarservice.ru assumes that the User provides true and
+              sufficient personal information and keeps such information up to
+              date. The consequences of providing false or insufficient
+              information are stipulated by the User Agreement for
+              onlinecarservice.ru services
+              (https://onlinecarservice.ru/user-agreement).
+            </li>
+            <li class="privacy__item">3</li>
             <ol class="privacy__list">
-              <li class="privacy__item">n</li>
+              <li class="privacy__item">4</li>
             </ol>
           </ol>
         </div>
