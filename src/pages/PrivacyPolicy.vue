e60604051fb699942634ccb88e085b0940c96a0a
Mizurkin <samyray2009@yandex.ru>
2022-09-15 15:50:49 +0300

    :rocket: add privacy list

--- src/pages/PrivacyPolicy.vue
+++ src/pages/PrivacyPolicy.vue
@@ -1,13 +1,53 @@
 <template>
-  <h1>Privacy Policy</h1>
+  <section>
+    <div class="container">
+      <div class="privacy">
+        <div class="privacy__title">
+          <h1 class="title">Privacy Policy</h1>
+        </div>
+        <div class="privacy__privacy">
+          <p>
+            The Personal Information Privacy Policy (hereinafter – Policy)
+            applies to any and all information about the User that DPF off
+            service or its affiliates, including all persons belonging to the
+            same group with DPF off service (hereinafter – onlinecarservice.ru),
+            may obtain in the course of the User’s use of any of
+            onlinecarservice.ru’s websites, services, programs, products or
+            services (hereinafter – Services, onlinecarservice.ru Services) and
+            during the performance of any agreements and contracts with the
+            User. The User’s agreement with this Policy within the framework of
+            relations with any of the persons listed above shall apply to all
+            persons listed above. By using the onlinecarservice.ru Services the
+            User expresses his/her unconditional agreement with this Policy and
+            the conditions of processing the user’s personal information
+            contained herein; if the User does not agree with these conditions,
+            the User should not use the Services.
+          </p>
+          <ol class="privacy__list">
+            <li class="privacy__item">User Personal Information Processed by onlinecarservice.ru</li>
+            <ol class="privacy__list">
+              <li class="privacy__item">According to this Policy, “User personal information” means the following:</li>
+              <ol class="privacy__list">
+                <li>Personal information provided by the User during registration (creation of an account) or in the course of using the Services, including the User’s personal details. Information required for the provision of the Services is marked respectively. Other information shall be provided by the User at his/her own discretion.</li>
+              </ol>
+            </ol>
+            <li class="privacy__item">n</li>
+            <li class="privacy__item">n</li>
+            <li class="privacy__item">n</li>
+            <ol class="privacy__list">
+              <li class="privacy__item">n</li>
+            </ol>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "PrivacyPolicy"
-}
+  name: "PrivacyPolicy",
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
