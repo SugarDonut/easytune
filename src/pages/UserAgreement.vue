975bcd03ea67ab8a75bd33bb71dad4ccad64b9f4
Mizurkin <samyray2009@yandex.ru>
2022-09-16 16:06:13 +0300

    :rocket: add agreement content

--- src/pages/UserAgreement.vue
+++ src/pages/UserAgreement.vue
@@ -1,13 +1,281 @@
 <template>
-  <h1>User Agreement</h1>
+  <section class="user-agreement bg--grad">
+    <div class="container">
+      <div class="agreement">
+        <div class="agreement__title">
+          <h1 class="title">User<br />Agreement</h1>
+        </div>
+        <div class="agreement__agreement">
+          <ol class="agreement__list">
+            <li class="agreement__item agreement__item--head">
+              General Provisions
+              <ol class="agreement__list">
+                <li class="agreement__item">
+                  onlinecarservice.ru offers Internet users (hereinafter – the
+                  “User”) the opportunity to use its services on the terms and
+                  conditions of this User Agreement (hereinafter - the
+                  “Agreement”, “User Agreement”). This Agreement shall take
+                  effect upon the User’s express agreement with its terms
+                  according to Clause 1.4 hereof.
+                </li>
+                <li class="agreement__item">
+                  onlinecarservice.ru offers Users access to services including
+                  uploading, analysis and modification of files. All currently
+                  available services as well as their development and/or
+                  addition of new services shall be the subject of this
+                  Agreement.
+                </li>
+                <li class="agreement__item">
+                  The use of onlinecarservice.ru services shall be regulated by
+                  this Agreement and Privacy Policy and terms of using certain
+                  services. This Agreement may be amended by onlinecarservice.ru
+                  without notice, and a new revision of this Agreement shall
+                  take effect when posted online at the web address given in
+                  this paragraph, unless otherwise stated in the new revision of
+                  this Agreement. The current revision of this User Agreement is
+                  always available at
+                  https://onlinecarservice.ru/user-agreement.
+                </li>
+                <li class="agreement__item">
+                  By starting the use of any services/certain functions or going
+                  through the registration procedure, the User shall be deemed
+                  to have accepted the terms of this Agreement in full without
+                  any limitations or exceptions. If the User does not agree to
+                  any provisions of this Agreement, the User must not use
+                  onlinecarservice.ru services. If onlinecarservice.ru makes any
+                  amendments to this Agreement as prescribed by Clause 1.3
+                  hereof to which the User does not agree, the User shall cease
+                  to use any onlinecarservice.ru services.
+                </li>
+              </ol>
+            </li>
+            <li class="agreement__item agreement__item--head">
+              Purpose of Processing User Personal Information
+              <ol class="agreement__list">
+                <li class="agreement__item">
+                  To use certain onlinecarservice.ru services or certain
+                  specific functions of services, the User shall complete
+                  registration to create a unique account.
+                </li>
+                <li class="agreement__item">
+                  Upon registration the User shall provide valid and complete
+                  information requested in the registration form and shall
+                  regularly update such information. If the User provides
+                  invalid information or onlinecarservice.ru has reasons to
+                  believe that any information provided by the User is
+                  incomplete or invalid, onlinecarservice.ru may at its
+                  discretion block or delete the User’s account or deny the User
+                  the use of any services (or certain functions).
+                </li>
+                <li class="agreement__item">
+                  onlinecarservice.ru shall reserve the right to require at any
+                  time that the User verify the information provided upon
+                  registration and to request supporting documents. Failure to
+                  provide these documents may, at onlinecarservice.ru's
+                  discretion, be regarded as provision of invalid information
+                  and entail consequences according to Clause 2.2 hereof. In
+                  case the User's information specified in the documents
+                  provided by him/her fails to conform to the information
+                  provided upon registration or if the information provided upon
+                  registration does not make it possible to identify the User,
+                  onlinecarservice.ru shall reserve the right to deny User's
+                  access to the User account and to usage of onlinecarservice.ru
+                  services.
+                </li>
+                <li class="agreement__item">
+                  Any personal information of the User contained in the User
+                  account shall be stored and processed by onlinecarservice.ru
+                  according to Privacy Policy
+                  (https://onlinecarservice.ru/privacy-policy).
+                </li>
+                <li class="agreement__item">
+                  Means of access to the User’s account.
+                  <ol class="agreement__list">
+                    <li class="agreement__item"></li>
+                  </ol>
+                </li>
+                <li class="agreement__item">
+                  The User shall be responsible for security of the chosen means
+                  of access to his/her account and for confidentiality of such
+                  means. The User shall be responsible for any actions (and
+                  their consequences) in and with onlinecarservice.ru services
+                  through the User’s account including voluntary transfer by the
+                  User of information required to access the User account to
+                  third parties on any conditions (including by contracts or
+                  agreements). Any actions in or with onlinecarservice.ru
+                  services performed through the User’s account shall be
+                  considered done by the User, except for the cases when the
+                  User as prescribed by Clause 2.7 hereof notifies
+                  onlinecarservice.ru of unauthorized access to
+                  onlinecarservice.ru services through the User’s account and/or
+                  any other breach (alleged breach) of confidentiality of the
+                  chosen means of access to his/her account.
+                </li>
+                <li class="agreement__item">
+                  The User shall promptly inform onlinecarservice.ru of any
+                  instances of unauthorized (not allowed by the User) access to
+                  onlinecarservice.ru services through the User’s account and/or
+                  any breach (alleged breach) of confidentiality of the chosen
+                  means of access to his/her account. For security reasons, the
+                  User shall securely log out after completing each session (Log
+                  Out button) of operating onlinecarservice.ru services.
+                  onlinecarservice.ru shall not be responsible for any potential
+                  loss or damage of information as well as any other
+                  consequences appearing as a result of failure by the User to
+                  comply with this part of this Agreement.
+                </li>
+                <li class="agreement__item">
+                  Account use by the User.
+                  <ol class="agreement__list">
+                    <li class="agreement__item"></li>
+                    <li class="agreement__item"></li>
+                  </ol>
+                </li>
+                <li class="agreement__item">
+                  Account termination.
+                  <ol class="agreement__list">
+                    <li class="agreement__item"></li>
+                  </ol>
+                </li>
+                <li class="agreement__item">
+                  User account deletion.
+                  <ol class="agreement__list">
+                    <li class="agreement__item"></li>
+                  </ol>
+                </li>
+              </ol>
+            </li>
+            <li class="agreement__item agreement__item--head">
+              General Usage and Storing Provisions
+              <ol class="agreement__list">
+                <li class="agreement__item">
+                  onlinecarservice.ru may impose restrictions on the use of
+                  services for all Users or certain categories of Users
+                  (depending on the User’s location, language of the service,
+                  etc.) including: availability/unavailability of certain
+                  service functions, period of storing tasks or files and any
+                  other content, maximum number of tasks or files by one
+                  registered user, maximum size of an file or disc space,
+                  maximum number of service uses in a certain period, maximum
+                  period of content storage, special parameters of downloadable
+                  content, etc. onlinecarservice.ru may prohibit automatic
+                  requests to its servers as well as terminate acceptance of any
+                  automatically generated information.
+                </li>
+                <li class="agreement__item">
+                  onlinecarservice.ru may send information messages to its
+                  users.
+                </li>
+              </ol>
+            </li>
+            <li class="agreement__item agreement__item--head">
+              Change and Deletion of Personal Information. Obligatory Data
+              Storage
+              <ol class="agreement__list">
+                <li class="agreement__item">
+                  The User may at any time change (update, supplement) the
+                  provided personal information or its part by editing his/her
+                  personal details in the personal section of a respective
+                  Service.
+                </li>
+                <li class="agreement__item">
+                  The User may also delete the personal information he has
+                  provided for a particular account by sending request to
+                  support service support@onlinecarservice.ru.
+                </li>
+                <li class="agreement__item">
+                  The rights under clauses 4.1. and 4.2. may be restricted in
+                  accordance with the legislative requirements. In particular,
+                  such restrictions may require that onlinecarservice.ru store
+                  the information changed or deleted by the User for a period
+                  specified by law and provide such information to governmental
+                  authorities according to a relevant legal procedure.
+                </li>
+              </ol>
+            </li>
+            <li class="agreement__item agreement__item--head">
+              Processing of Personal Information by Using Cookies and Counters
+              <ol class="agreement__list">
+                <li class="agreement__item">
+                  Cookies transferred by onlinecarservice.ru to the User’s
+                  devices and by the User’s devices to onlinecarservice.ru may
+                  be used by onlinecarservice.ru to provide customized Services
+                  to the User, to target advertising displayed to the User, for
+                  statistical and research purposes and to improve the
+                  onlinecarservice.ru Services.
+                </li>
+                <li class="agreement__item">
+                  The User understands that hardware and software he/she uses to
+                  visit websites may have an option for blocking any actions
+                  with cookies (for any websites or for certain websites) and
+                  for deletion of previously received cookies.
+                </li>
+                <li class="agreement__item">
+                  onlinecarservice.ru may determine that provision of a
+                  particular Service is available only subject to the User’s
+                  consent to acceptance and receipt of cookies.
+                </li>
+                <li class="agreement__item">
+                  Structure, content and technical parameters of any cookie
+                  shall be determined by onlinecarservice.ru and may be changed
+                  without prior notice to the User.
+                </li>
+                <li class="agreement__item">
+                  Counters placed by onlinecarservice.ru within the Services may
+                  be used to analyze cookies of the User, to collect and process
+                  statistical information on the
+                </li>
+                <li class="agreement__item">
+                  Service use, and to ensure the performance capacity of the
+                  Services, in general, and their certain functions, in
+                  particular. Technical parameters of counter performance shall
+                  be determined by onlinecarservice.ru and may be changed
+                  without prior notice to the User.
+                </li>
+              </ol>
+            </li>
+            <li class="agreement__item agreement__item--head">
+              User Personal Information Protection Measures
+              <ol class="agreement__list">
+                <li class="agreement__item">
+                  onlinecarservice.ru shall take necessary and sufficient
+                  organizational and technical measures to protect User personal
+                  information from unlawful or accidental access, deletion,
+                  alteration, blocking, copying, distribution, as well as from
+                  unlawful actions of third parties.
+                </li>
+                <li class="agreement__item">
+                  Your personal information is contained behind secured networks
+                  and is only accessible by a limited number of persons who have
+                  special access rights to such systems, and are required to
+                  keep the information confidential. In addition, all sensitive
+                  information you supply is encrypted via Secure Socket Layer
+                  (SSL) technology.
+                </li>
+                <li class="agreement__item">
+                  All transactions are processed through a gateway provider and
+                  are not stored or processed on our servers. An external PCI
+                  compliant payment gateway handles all credit card
+                  transactions. We do not store credit card information.
+                </li>
+                <li class="agreement__item">
+                  We implement a variety of security measures when a user places
+                  an order enters, submits, or accesses their information to
+                  maintain the safety of your personal information.
+                </li>
+              </ol>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "UserAgreement"
-}
+  name: "UserAgreement",
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
