523aa4b1d4b9b801dd99a28544fae4074571ccdb
Mizurkin <samyray2009@yandex.ru>
2022-09-16 14:02:51 +0300

    :rocket: add privacy content

--- src/pages/PrivacyPolicy.vue
+++ src/pages/PrivacyPolicy.vue
@@ -1,9 +1,9 @@
 <template>
-  <section>
+  <section class="privacy-policy bg--grad">
     <div class="container">
       <div class="privacy">
         <div class="privacy__title">
-          <h1 class="title">Privacy<br>Policy</h1>
+          <h1 class="title">Privacy<br />Policy</h1>
         </div>
         <div class="privacy__privacy">
           <p class="privacy__descr">
@@ -23,63 +23,242 @@
             contained herein; if the User does not agree with these conditions,
             the User should not use the Services.
           </p>
-          <ol class="privacy__list ">
-            <li class="privacy__item privacy__list--head">
+          <ol class="privacy__list">
+            <li class="privacy__item privacy__item--head">
               User Personal Information Processed by onlinecarservice.ru
-            </li>
-            <ol class="privacy__list">
-              <li class="privacy__item">
-                According to this Policy, “User personal information” means the
-                following:
-              </li>
               <ol class="privacy__list">
                 <li class="privacy__item">
-                  Personal information provided by the User during registration
-                  (creation of an account) or in the course of using the
-                  Services, including the User’s personal details. Information
-                  required for the provision of the Services is marked
-                  respectively. Other information shall be provided by the User
-                  at his/her own discretion.
+                  According to this Policy, “User personal information” means
+                  the following:
+                  <ol class="privacy__list">
+                    <li class="privacy__item">
+                      Personal information provided by the User during
+                      registration (creation of an account) or in the course of
+                      using the Services, including the User’s personal details.
+                      Information required for the provision of the Services is
+                      marked respectively. Other information shall be provided
+                      by the User at his/her own discretion.
+                    </li>
+                    <li class="privacy__item">
+                      Data automatically transferred by the software installed
+                      on the User’s device to the onlinecarservice.ru Services
+                      in the course of their use, including, but not limited to,
+                      IP address, cookies data, the User’s browser data (or any
+                      other software used to access the Services), performance
+                      specifications of the User’s hardware and software, date
+                      and time of access to the Services, the URL of the page
+                      requested and other similar information.
+                    </li>
+                    <li class="privacy__item">
+                      Other information about the User required to be processed
+                      according to the terms and conditions governing the use of
+                      particular onlinecarservice.ru Services.
+                    </li>
+                  </ol>
                 </li>
                 <li class="privacy__item">
-                  Data automatically transferred by the software installed on
-                  the User’s device to the onlinecarservice.ru Services in the
-                  course of their use, including, but not limited to, IP
-                  address, cookies data, the User’s browser data (or any other
-                  software used to access the Services), performance
-                  specifications of the User’s hardware and software, date and
-                  time of access to the Services, the URL of the page requested
-                  and other similar information.
+                  This Policy only applies to the information required to be
+                  processed during the use of onlinecarservice.ru Services.
+                  onlinecarservice.ru has no control over, and does not bear
+                  responsibility for, processing of the information by
+                  third-party websites, which the User may access by hyperlinks
+                  available on onlinecarservice.ru’s websites.
                 </li>
                 <li class="privacy__item">
-                  Other information about the User required to be processed
-                  according to the terms and conditions governing the use of
-                  particular onlinecarservice.ru Services.
+                  onlinecarservice.ru will not verify personal information
+                  provided by the User and cannot judge on his/her legal
+                  capacity. However, onlinecarservice.ru assumes that the User
+                  provides true and sufficient personal information and keeps
+                  such information up to date. The consequences of providing
+                  false or insufficient information are stipulated by the User
+                  Agreement for onlinecarservice.ru services
+                  (https://onlinecarservice.ru/user-agreement).
                 </li>
               </ol>
-            </ol>
-            <li class="privacy__item">
-              This Policy only applies to the information required to be
-              processed during the use of onlinecarservice.ru Services.
-              onlinecarservice.ru has no control over, and does not bear
-              responsibility for, processing of the information by third-party
-              websites, which the User may access by hyperlinks available on
-              onlinecarservice.ru’s websites.
             </li>
-            <li class="privacy__item">
-              onlinecarservice.ru will not verify personal information provided
-              by the User and cannot judge on his/her legal capacity. However,
-              onlinecarservice.ru assumes that the User provides true and
-              sufficient personal information and keeps such information up to
-              date. The consequences of providing false or insufficient
-              information are stipulated by the User Agreement for
-              onlinecarservice.ru services
-              (https://onlinecarservice.ru/user-agreement).
+            <li class="privacy__item privacy__item--head">
+              Purpose of Processing User Personal Information
+              <ol class="privacy__list">
+                <li class="privacy__item">
+                  onlinecarservice.ru only collects and stores personal
+                  information required for the provision of the Services (or
+                  execution of agreements and contracts with the User), except
+                  in cases where such information is required to be stored by
+                  law for a period prescribed by law.
+                </li>
+                <li class="privacy__item">
+                  onlinecarservice.ru will process the User personal information
+                  in order to:
+                  <ol class="privacy__list">
+                    <li class="privacy__item">
+                      Identify a party using the Services or a party to an
+                      agreement or contract with onlinecarservice.ru;
+                    </li>
+                    <li class="privacy__item">
+                      Provide personalized Services to the User and perform
+                      agreements and contracts;
+                    </li>
+                    <li class="privacy__item">
+                      Contact the User, as well as forward notices, requests and
+                      information related to the onlinecarservice.ru Services
+                      and performance of agreements and contracts, and process
+                      requests and applications of the User;
+                    </li>
+                    <li class="privacy__item">
+                      Improve the quality of the Services, convenience of their
+                      use, develop new Services;
+                    </li>
+                    <li class="privacy__item">Target advertisements;</li>
+                    <li class="privacy__item">
+                      Carry out statistical and other kinds of research and
+                      analysis based on anonymized data.
+                    </li>
+                  </ol>
+                </li>
+              </ol>
             </li>
-            <li class="privacy__item">3</li>
-            <ol class="privacy__list">
-              <li class="privacy__item">4</li>
-            </ol>
+            <li class="privacy__item privacy__item--head">
+              Conditions of Processing User Personal Information and Its
+              Provision to Third Parties
+              <ol class="privacy__list">
+                <li class="privacy__item">
+                  onlinecarservice.ru shall store User personal information
+                  according to internal regulations on particular services.
+                </li>
+                <li class="privacy__item">
+                  The confidentiality of User personal information shall be
+                  maintained.
+                </li>
+                <li class="privacy__item">
+                  onlinecarservice.ru has the right to provide User personal
+                  information to third parties subject to the following
+                  conditions:
+                  <ol class="privacy__list">
+                    <li class="privacy__item">
+                      The User has expressed his/her consent to such actions;
+                    </li>
+                    <li class="privacy__item">
+                      Such provision takes place as part of the sale or other
+                      transfer of the business (in full or in part), and all
+                      obligations under this Policy in relation to any personal
+                      information that the buyer may obtain shall be transferred
+                      to the buyer;
+                    </li>
+                    <li class="privacy__item">
+                      In order to secure remedy for onlinecarservice.ru or third
+                      parties in the event of a breach by the User of the User
+                      Agreement for onlinecarservice.ru Services
+                      (https://onlinecarservice.ru/user-agreement), this Policy
+                      or documents governing the use of particular Services;
+                    </li>
+                    <li class="privacy__item">
+                      Anonymized statistical data generated as a result of
+                      processing of the User personal information by way of
+                      anonymization are provided to a third party to carry out
+                      research activities, perform works or render services as
+                      requested by onlinecarservice.ru.
+                    </li>
+                  </ol>
+                </li>
+              </ol>
+            </li>
+            <li class="privacy__item privacy__item--head">
+              Change and Deletion of Personal Information. Obligatory Data
+              Storage
+              <ol class="privacy__list">
+                <li class="privacy__item">
+                  The User may at any time change (update, supplement) the
+                  provided personal information or its part by editing his/her
+                  personal details in the personal section of a respective
+                  Service.
+                </li>
+                <li class="privacy__item">
+                  The User may also delete the personal information he has
+                  provided for a particular account by sending request to
+                  support service support@onlinecarservice.ru.
+                </li>
+                <li class="privacy__item">
+                  The rights under clauses 4.1. and 4.2. may be restricted in
+                  accordance with the legislative requirements. In particular,
+                  such restrictions may require that onlinecarservice.ru store
+                  the information changed or deleted by the User for a period
+                  specified by law and provide such information to governmental
+                  authorities according to a relevant legal procedure.
+                </li>
+              </ol>
+            </li>
+            <li class="privacy__item privacy__item--head">
+              Processing of Personal Information by Using Cookies and Counters
+              <ol class="privacy__list">
+                <li class="privacy__item">
+                  Cookies transferred by onlinecarservice.ru to the User’s
+                  devices and by the User’s devices to onlinecarservice.ru may
+                  be used by onlinecarservice.ru to provide customized Services
+                  to the User, to target advertising displayed to the User, for
+                  statistical and research purposes and to improve the
+                  onlinecarservice.ru Services.
+                </li>
+                <li class="privacy__item">
+                  The User understands that hardware and software he/she uses to
+                  visit websites may have an option for blocking any actions
+                  with cookies (for any websites or for certain websites) and
+                  for deletion of previously received cookies.
+                </li>
+                <li class="privacy__item">
+                  onlinecarservice.ru may determine that provision of a
+                  particular Service is available only subject to the User’s
+                  consent to acceptance and receipt of cookies.
+                </li>
+                <li class="privacy__item">
+                  Structure, content and technical parameters of any cookie
+                  shall be determined by onlinecarservice.ru and may be changed
+                  without prior notice to the User.
+                </li>
+                <li class="privacy__item">
+                  Counters placed by onlinecarservice.ru within the Services may
+                  be used to analyze cookies of the User, to collect and process
+                  statistical information on the
+                </li>
+                <li class="privacy__item">
+                  Service use, and to ensure the performance capacity of the
+                  Services, in general, and their certain functions, in
+                  particular. Technical parameters of counter performance shall
+                  be determined by onlinecarservice.ru and may be changed
+                  without prior notice to the User.
+                </li>
+              </ol>
+            </li>
+            <li class="privacy__item privacy__item--head">
+              User Personal Information Protection Measures
+              <ol class="privacy__list">
+                <li class="privacy__item">
+                  onlinecarservice.ru shall take necessary and sufficient
+                  organizational and technical measures to protect User personal
+                  information from unlawful or accidental access, deletion,
+                  alteration, blocking, copying, distribution, as well as from
+                  unlawful actions of third parties.
+                </li>
+                <li class="privacy__item">
+                  Your personal information is contained behind secured networks
+                  and is only accessible by a limited number of persons who have
+                  special access rights to such systems, and are required to
+                  keep the information confidential. In addition, all sensitive
+                  information you supply is encrypted via Secure Socket Layer
+                  (SSL) technology.
+                </li>
+                <li class="privacy__item">
+                  All transactions are processed through a gateway provider and
+                  are not stored or processed on our servers. An external PCI
+                  compliant payment gateway handles all credit card
+                  transactions. We do not store credit card information.
+                </li>
+                <li class="privacy__item">
+                  We implement a variety of security measures when a user places
+                  an order enters, submits, or accesses their information to
+                  maintain the safety of your personal information.
+                </li>
+              </ol>
+            </li>
           </ol>
         </div>
       </div>
